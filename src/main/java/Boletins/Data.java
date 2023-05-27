3e5c5968f39c8ea405d09e8c45018afadb8b4f38
Administrador <flavio.lehmann@educacao.gov.br>
2023-05-27 11:03:49 -0300

    Ver primeira versão estável

---
+++ src/main/java/Boletins/Data.java
@@ -0,0 +1,30 @@
+package Boletins;
+
+public class Data {
+    private String id;
+    private String value1, value2, value3;
+
+    public Data(String id, String value) {
+        this.id = id;
+        this.value1 = value;
+        this.value2 = value;
+        this.value3 = value;
+    }
+
+    public String getId() {
+        return id;
+
+
+    }
+
+    public String getValue() {
+        return value1;
+
+
+
+    }
+
+
+
+
+}
